94ce1efa03e7b01e3dd5577611bf7d594363b721
Tesla32 <frolovso92@gmail.com>
2023-02-26 13:41:57 +0200

    Revert "Chenge xpath"
    This reverts commit b8de69119f7a76ece2703d6501d060a0fc7aaf34.

--- Fralou_Siarhei_HW_20/test_web.py
+++ Fralou_Siarhei_HW_20/test_web.py
@@ -7,9 +7,9 @@ def test_web():
     url = 'https://google.com'
     chrome.get(url=url)
     #chrome.maximize_window()
-    accept_button = chrome.find_element(By.XPATH, "//div[text()='Atmesti viską']")
+    accept_button = chrome.find_element(By.XPATH, "//*[@id='W0wltc']/div")
     accept_button.click()
-    search_box = chrome.find_element(By.CSS_SELECTOR, "[aria-autocomplete='both']")
+    search_box = chrome.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
     search_box.send_keys('python 3.10')
     search_box.submit()
     search_element = chrome.find_element(By.XPATH, "//*[@id='rso']/div[1]/div/div/div/div[1]/div/a/h3")
